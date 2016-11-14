e57b056aaeb31fc8287972828b1b0fcb4f0a1096
cpeg <coreydarkblades@yahoo.com>
2016-11-14 16:26:33 -0600

    Initial Commit

---
+++ Git-ICE_CPE/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Git-ICE_CPE
+//
+//  Created by Ehlers,Corey P on 11/14/16.
+//  Copyright © 2016 Ehlers,Corey P. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
